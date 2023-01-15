f837ff7e868a033dafcfddde4029758c1bc00e7f
Ayub_Che <ajubchechiev@mail.ru>
2023-01-15 20:33:48 +0300

    initial commit

---
+++ src/components/Counter.tsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import classes from "./Counter.module.scss";
+const Counter = () => {
+    const  [count,setCount] = useState(0);
+
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button className={classes.btn} onClick={()=>{setCount(count+1)}}>Inc</button>
+        </div>
+    );
+};
+
+export default Counter;
