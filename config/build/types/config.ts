7f956aac7ecdea25a02aa984c558d80077417c1d
Ayub_Che <ajubchechiev@mail.ru>
2023-01-17 20:16:56 +0300

    added i18n and eslint

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -1,21 +1,20 @@
-
-
 export type BuildMode = 'production' | 'development';
 
 export interface BuildPaths {
     entry: string;
-    build:string;
-    html:string;
+    build: string;
+    html: string;
+    src: string;
 }
 
 export interface BuildEnv {
     mode: BuildMode;
-    port:number;
+    port: number;
 }
 
 export interface BuildOptions {
-    mode:BuildMode;
+    mode: BuildMode;
     paths: BuildPaths;
-    isDev:boolean;
+    isDev: boolean;
     port: number;
 }
