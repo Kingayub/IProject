beb206fde10ffc58a117d6c625f6a11f40baa2f2
Ayub_Che <ajubchechiev@mail.ru>
2023-01-27 22:11:30 +0300

    fix screens....

--- src/entities/Profile/model/services/updateProfileData/updateProfileData.ts
+++ src/entities/Profile/model/services/updateProfileData/updateProfileData.ts
@@ -1,8 +1,8 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { ThunkConfig } from 'app/providers/StoreProvider';
 import { validateProfileData } from 'entities/Profile/model/services/validateProfileData/validateProfileData';
+import { getProfileForm } from 'entities/Profile';
 import { Profile, ValidateProfileError } from '../../types/profile';
-import { getProfileForm } from '../../selectors/getProfileForm/getProfileForm';
 
 export const updateProfileData = createAsyncThunk<
     Profile,
@@ -28,7 +28,6 @@ export const updateProfileData = createAsyncThunk<
             }
             return response.data;
         } catch (e) {
-            console.log(e);
             return rejectWithValue([ValidateProfileError.SERVER_ERROR]);
         }
     },
