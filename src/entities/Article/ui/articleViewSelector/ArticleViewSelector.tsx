3f375bf3a0e0e772ff75f1c67df0bf67759baa15
Ayub_Che <ajubchechiev@mail.ru>
2023-02-01 17:14:06 +0300

    add fetching articles view type selector

---
+++ src/entities/Article/ui/articleViewSelector/ArticleViewSelector.tsx
@@ -0,0 +1,47 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import ListIcon from 'shared/assets/icons/list.svg';
+import TiledIcon from 'shared/assets/icons/tiled.svg';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { Icon } from 'shared/ui/Icon/Icon';
+import cls from './ArticleViewSelector.module.scss';
+import { ArticleView } from '../../model/types/article';
+
+interface articleViewSelectorProps {
+    className?: string;
+    view: ArticleView,
+    onViewClick?: (newView:ArticleView)=> void;
+}
+
+const viewTypes = [
+    {
+        view: ArticleView.SMALL,
+        icon: ListIcon,
+    },
+    {
+        view: ArticleView.BIG,
+        icon: TiledIcon,
+    },
+];
+
+export const ArticleViewSelector = (props: articleViewSelectorProps) => {
+    const { className, view, onViewClick } = props;
+
+    const onClick = (newView: ArticleView) => () => {
+        onViewClick?.(newView);
+    };
+    return (
+        <div className={classNames(cls.ArticleViewSelector, {}, [className])}>
+            {viewTypes.map((viewType) => (
+                <Button
+                    theme={ButtonTheme.CLEAR}
+                    onClick={onClick(viewType.view)}
+                >
+                    <Icon
+                        Svg={viewType.icon}
+                        className={classNames('', { [cls.notSelected]: viewType.view !== view }, [])}
+                    />
+                </Button>
+            ))}
+        </div>
+    );
+};
--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -2,19 +2,17 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { memo, useCallback } from 'react';
 import { ArticleDetails } from 'entities/Article';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { Text } from 'shared/ui/Text/Text';
 import { CommentList } from 'entities/Comment';
 import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { useDispatch, useSelector } from 'react-redux';
 import { useInitialEffect } from 'shared/lib/hooks/useInnitialEffect/useInitialEffect';
 import { AddCommentForm } from 'features/addCommentForm';
-import {
-    fetchCommentsByArticleId,
-} from '../../model/services/fetchCommentsByArticleId/fetchCommentsByArticleId';
-import {
-    addCommentForArticle,
-} from '../../model/services/addCommentForArticle/addCommentForArticle';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import { fetchCommentsByArticleId } from '../../model/services/fetchCommentsByArticleId/fetchCommentsByArticleId';
+import { addCommentForArticle } from '../../model/services/addCommentForArticle/addCommentForArticle';
 import { articleDetailsCommentReducer, getArticleComments } from '../../model/slices/articleDetailsCommentSlice';
 import cls from './ArticleDetailsPage.module.scss';
 import { getArticleCommentsIsLoading } from '../../model/selectors/comments';
@@ -33,12 +31,16 @@ const ArticleDetailsPage = ({ className }: ArticleDetailsPageProps) => {
     const comments = useSelector(getArticleComments.selectAll);
     const commentsIsLoading = useSelector(getArticleCommentsIsLoading);
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const onSendComment = useCallback((text) => {
         dispatch(addCommentForArticle(text));
     }, [dispatch]);
     useInitialEffect(() => {
         dispatch(fetchCommentsByArticleId(id));
     });
+    const onBackToList = useCallback(() => {
+        navigate(RoutePath.articles);
+    }, [navigate]);
 
     if (!id) {
         return (
@@ -50,6 +52,12 @@ const ArticleDetailsPage = ({ className }: ArticleDetailsPageProps) => {
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
             <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+                <Button
+                    theme={ButtonTheme.OUTLINE}
+                    onClick={onBackToList}
+                >
+                    {t('Назад к списку')}
+                </Button>
                 <ArticleDetails id={id} />
                 <Text className={cls.commentTitle} title={t('Комментарии')} />
                 <AddCommentForm onSendComment={onSendComment} />
