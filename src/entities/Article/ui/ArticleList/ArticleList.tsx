4fb7c896cdc207c0337e4e808e9d1492d708906b
Ayub_Che <ajubchechiev@mail.ru>
2023-02-16 02:51:05 +0300

    add react-virtualized

--- src/entities/Article/ui/ArticleList/ArticleList.tsx
+++ src/entities/Article/ui/ArticleList/ArticleList.tsx
@@ -3,6 +3,8 @@ import { useTranslation } from 'react-i18next';
 import { Article, ArticleView } from 'entities/Article';
 import { HTMLAttributeAnchorTarget, memo } from 'react';
 import { Text, TextSize } from 'shared/ui/Text/Text';
+import { List, ListRowProps, WindowScroller } from 'react-virtualized';
+import { PAGE_ID } from 'widgets/Page/Page';
 import { ArticleListItem } from '../ArticleListItem/ArticleListItem';
 import cls from './ArticleList.module.scss';
 import { ArticleListItemSkeleton } from '../ArticleListItem/ArticleListItemSkeleton';
@@ -31,15 +33,37 @@ export const ArticleList = memo((props: ArticleListProps) => {
     } = props;
     const { t } = useTranslation();
 
-    const renderArticle = (article:Article) => (
-        <ArticleListItem
-            article={article}
-            view={view}
-            className={cls.card}
-            key={article.id}
-            target={target}
-        />
-    );
+    const isBig = view === ArticleView.BIG;
+    const itemsPerRow = isBig ? 1 : 3;
+    const rowCount = isBig ? 1 : Math.ceil(articles.length / itemsPerRow);
+
+    const rowRenderer = ({
+        index, isScrolling, key, style,
+    }: ListRowProps) => {
+        const items = [];
+        const fromIndex = index * itemsPerRow;
+        const toIndex = Math.min(fromIndex + itemsPerRow, articles.length);
+        for (let i = fromIndex; i < toIndex; i += 1) {
+            items.push(
+                <ArticleListItem
+                    article={articles[index]}
+                    view={view}
+                    className={cls.card}
+                    target={target}
+                    key={`str${i}`}
+                />,
+            );
+        }
+        return (
+            <div
+                key={key}
+                className={cls.row}
+                style={style}
+            >
+                {items}
+            </div>
+        );
+    };
 
     if (isLoading && !articles.length) {
         return (
@@ -50,11 +74,43 @@ export const ArticleList = memo((props: ArticleListProps) => {
     }
 
     return (
-        <div className={classNames(cls.ArticleList, {}, [className, cls[view]])}>
-            {articles.length > 0
-                ? articles.map(renderArticle)
-                : null}
-            {isLoading && getSkeletons(view)}
-        </div>
+        <WindowScroller
+            scrollElement={document.getElementById(PAGE_ID) as Element}
+        >
+            {(
+                {
+                    height,
+                    width,
+                    registerChild,
+                    isScrolling,
+                    scrollTop,
+                    onChildScroll,
+                },
+            ) => (
+                <div
+                    ref={registerChild}
+                    className={classNames(cls.ArticleList, {}, [className, cls[view]])}
+                >
+                    <List
+                        height={height ?? 700}
+                        rowCount={rowCount}
+                        rowHeight={isBig ? 700 : 330}
+                        rowRenderer={rowRenderer}
+                        width={width ? width - 80 : 700}
+                        autoHeight
+                        onScroll={onChildScroll}
+                        isScrolling={isScrolling}
+                        scrollTop={scrollTop}
+                    />
+                    {isLoading && getSkeletons(view)}
+                </div>
+            )}
+        </WindowScroller>
+
+    //     {articles.length > 0
+    //         ? articles.map(renderArticle)
+    //         : null}
+    //
+    //
     );
 });
