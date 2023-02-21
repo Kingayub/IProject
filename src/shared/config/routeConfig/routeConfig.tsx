fc7fead548365cf21103e82be75efc6339e6cc80
Ayub_Che <ajubchechiev@mail.ru>
2023-02-21 22:55:32 +0300

    add private route with required roles

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -7,9 +7,13 @@ import { ProfilePage } from 'pages/ProfilePage';
 import { ArticlesPage } from 'pages/ArticlesPage';
 import { ArticleDetailsPage } from 'pages/ArticleDetailsPage';
 import { ArticleEditPage } from 'pages/ArticleEditPage';
+import { AdminPanelPage } from 'pages/AdminPanelPage';
+import { UserRole } from 'entities/User';
+import { ForbiddenPage } from 'pages/ForbiddenPage';
 
 export type AppRouteProps = RouteProps & {
     authOnly?: boolean;
+    roles?: UserRole[];
 }
 
 export enum AppRoutes {
@@ -20,6 +24,8 @@ export enum AppRoutes {
     ARTICLE_DETAILS = 'article_details',
     ARTICLE_CREATE = 'article_create',
     ARTICLE_EDIT = 'article_edit',
+    ADMIN_PANEL = 'admin_panel',
+    FORBIDDEN_PAGE = 'forbidden',
 
     // last
     NOT_FOUND = 'not_found',
@@ -33,7 +39,8 @@ export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.ARTICLE_DETAILS]: '/articles/', // +id
     [AppRoutes.ARTICLE_CREATE]: '/articles/new',
     [AppRoutes.ARTICLE_EDIT]: '/articles/:id/edit',
-
+    [AppRoutes.ADMIN_PANEL]: '/admin',
+    [AppRoutes.FORBIDDEN_PAGE]: '/forbidden',
     // last
     [AppRoutes.NOT_FOUND]: '*',
 };
@@ -72,6 +79,17 @@ export const routeConfig: Record<AppRoutes, AppRouteProps> = {
         element: <ArticleEditPage />,
         authOnly: true,
     },
+    [AppRoutes.ADMIN_PANEL]: {
+        path: `${RoutePath.admin_panel}`,
+        element: <AdminPanelPage />,
+        authOnly: true,
+        roles: [UserRole.MANAGER, UserRole.ADMIN],
+    },
+    [AppRoutes.FORBIDDEN_PAGE]: {
+        path: `${RoutePath.forbidden}`,
+        element: <ForbiddenPage />,
+    },
+
     // last
     [AppRoutes.NOT_FOUND]: {
         path: RoutePath.not_found,
