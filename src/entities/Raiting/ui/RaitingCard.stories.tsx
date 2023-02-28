e9d76ad729409226803dbfbb570652cfe4802656
Ayub_Che <ajubchechiev@mail.ru>
2023-03-01 01:20:59 +0300

    add star rating add Rating entity and rating card

---
+++ src/entities/Raiting/ui/RaitingCard.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { RaitingCard } from './RaitingCard';
+
+export default {
+    title: 'shared/RaitingCard',
+    component: RaitingCard,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof RaitingCard>;
+
+const Template: ComponentStory<typeof RaitingCard> = (args) => <RaitingCard {...args} />;
+
+export const Normal = Template.bind({});
+Normal.args = {};
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -3,6 +3,8 @@ import { useTranslation } from 'react-i18next';
 import { Page } from '@/widgets/Page/Page';
 import { HStack } from '@/shared/ui/Stack';
 import { ListBox } from '@/shared/ui/Popups/ui/ListBox/ListBox';
+import { StarRating } from '@/shared/ui/StarRating/StarRating';
+import { RaitingCard } from '@/entities/Raiting';
 
 const MainPage = memo(() => {
     const { t } = useTranslation();
@@ -10,6 +12,7 @@ const MainPage = memo(() => {
     return (
         <Page>
             {t('Главная страница')}
+            <RaitingCard />
         </Page>
     );
 });
